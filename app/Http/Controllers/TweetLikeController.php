15b49bc806b9c7ddefd4bb4c2f7c087ef0af5e68
kaofuru <hnks.ff@gmail.com>
2024-02-04 10:51:57 +0900

    new add

---
+++ app/Http/Controllers/TweetLikeController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+
+
+// class LikeController extends Controller
+// {
+//     /**
+//      * Store a newly created resource in storage.
+//      */
+//     public function store(Tweet $tweet)
+//     {
+//         $tweet->liked()->attach(auth()->id());
+//         return back();
+//     }
+
+//     /**
+//      * Remove the specified resource from storage.
+//      */
+//     public function destroy(Tweet $tweet)
+//     {
+//         $tweet->liked()->detach(auth()->id());
+//         return back();
+//     }
+// }
+
+class TweetLikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Tweet $tweet)
+    {
+        $tweet->liked()->attach(auth()->id());
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tweet $tweet)
+    {
+        $tweet->liked()->detach(auth()->id());
+        return back();
+    }
+}
